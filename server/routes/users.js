a946939be92a6cb162e25a79f3a4a89006aa6f85
Jambo <jambotoyou@gmail.com>
2019-09-27 22:38:37 +0400

    got access token by code

--- server/routes/users.js
+++ server/routes/users.js
@@ -17,8 +17,14 @@ router.get('/auth', function(req, res, next) {
         client_secret: "UvqccxvCevnGOxsjoqFy",
         client_id: "7150584"
       }
-  }).then(json => console.log(json)).catch(json => console.log(json));
-  res.send("huy");
+  }).then(json =>
+      res.send(JSON.stringify({
+        status: "success",
+        access_token: json.access_token })))
+    .catch(err =>
+      res.send({
+        status: "error"
+      }));
 });
 
 module.exports = router;
