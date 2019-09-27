7eba2ed614a7eca544b07ddb43a32f1586b82bb7
Jambo <jambotoyou@gmail.com>
2019-09-27 21:23:32 +0400

    auth route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,23 @@
+var axios = require('axios');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/auth', function(req, res, next) {
+  let code = req.params["code"];
+  axios.get("https://oauth.vk.com/access_token", {
+    params: {
+      code: code,
+      redirect_uri: "http://localhost:8080/redirect",
+      client_secret: "UvqccxvCevnGOxsjoqFy",
+      client_id: "7150584"
+    }
+  }).then(json => console.log(json));
+  res.send("huy");
 });
 
 module.exports = router;
