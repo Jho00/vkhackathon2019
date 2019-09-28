bd35182bc14ef396698e71c7f69eb66c9312a7fb
Jambo <jambotoyou@gmail.com>
2019-09-28 09:22:20 +0400

    finished registration with adding user to db

---
+++ server/routes/dbinfo.js
@@ -0,0 +1,8 @@
+const db = "local";
+const connStr = "mongodb://localhost:27017/"
+const challengesCollection = "challenges";
+const usersCollection = "users";
+// const Collection = "challenges";
+// const Collection = "challenges";
+
+module.exports = { db, challengesCollection, usersCollection, connStr }
