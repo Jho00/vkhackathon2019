4047b1a98b01d91262d321f709005018b7faa9b3
Jho00 <akutin_artem@mail.ru>
2019-09-29 00:58:36 +0400

    main page design

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -2,14 +2,23 @@
   <div class="home">
     <div v-if="$store.getters.isLogined">
       <h1>ОРБИ Social</h1>
-      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At delectus dolores doloribus esse et, eveniet
-        exercitationem facere fugit inventore maiores minus molestiae mollitia nam necessitatibus officia omnis
-        perspiciatis ut voluptatum!</p>
+      <p>ОРБИ Social - это платформа, которая поможет привлечь внимание общества к проблеме инсульта, популяризировать благотворительную деятельность, а также улучшить здоровье и снизить риск возникновения инсульта.</p>
 
-      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A animi autem commodi dolorum et,
-        excepturi, illum iste iure magni maxime nam nesciunt possimus quae quod sapiente sint sit voluptas voluptatibus.
-        Amet animi asperiores aspernatur aut, blanditiis deserunt dicta distinctio esse excepturi maiores minima modi
-        perferendis porro quaerat quasi ut voluptatibus?</p>
+      <p>Неравнодушные люди смогут донести до масс, что инсульт - проблема, которая может коснуться каждого из нас и любой из наших близких не застрахован.</p>
+      <p>Около половины, перенесших сосудистую катастрофу, погибают в течение года. Лишь 10% из выживших полностью восстанавливаются и могут продолжить работать. Остальные становятся инвалидами, часто неспособными обслуживать себя. Треть перенесших ОНМК испытывает постинсультную депрессию. Две трети - страх повторного эпизода. Более половины - отмечают, что члены семьи и друзья стали относиться к ним иначе. Столько же признают, что не могут заботиться о семье так же, как и раньше.</p>
+      <p>Каждые 90 секунд с одним из жителей нашей страны происходит острое нарушение мозгового кровообращения (ОНМК).
+        В течение суток в городские больницы Москвы поступает более ста пациентов с инсультом.
+        Каждый год около полумиллиона россиян переносит ОНМК.</p>
+
+      <br>
+
+      <el-steps :active="active" direction="vertical"  style="height: 300px;" finish-status="success">
+        <el-step title="Создавайте испытания"></el-step>
+        <el-step title="Присоединяйтесь к испытаниям с витрины"></el-step>
+        <el-step title="Зарабатывайте коины и поддерживайте свое здоровье"></el-step>
+      </el-steps>
+
+      <el-button style="margin-top: 12px;" @click="next">Далее</el-button>
     </div>
     <div v-if="!$store.getters.isLogined">
         <el-row>
@@ -38,7 +47,13 @@ export default Vue.extend({
   name: 'home',
   data() {
     return {
-      redirect_uri: `http://127.0.0.1:8080/redirect`
+      redirect_uri: `http://127.0.0.1:8080/redirect`,
+      active: 0
+    }
+  },
+  methods: {
+    next() {
+      if (this.active++ > 2) this.active = 0;
     }
   }
 });
@@ -46,8 +61,11 @@ export default Vue.extend({
 
 <style scoped lang="less">
   .home {
-    padding-top: 20px;
+    padding: 20px;
+
+    p{ text-align: justify}
   }
+
 
   .bottom {
     margin-top: 13px;
