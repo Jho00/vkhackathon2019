88c4162c9607248d2e7923ec465f66bcd48ccb98
Jho00 <akutin_artem@mail.ru>
2019-09-28 12:09:36 +0400

    add user money

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 var dbinfo = require('./dbinfo');
 var DBClinet = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -66,6 +67,48 @@ router.get('/auth', function (req, res, next) {
 				status: "error"
 			});
 		});
+});
+
+router.post('/add-money', function (req, res, next) {
+	const count = Number(req.query.count);
+	const id = req.query.user_id;
+	const sendOk = function() {
+		res.send({status: "ok"});
+	};
+	const oid = new ObjectId(id);
+	DBClinet.connect(dbinfo.connStr, function(err, userClient) {
+		userClient
+			.db(dbinfo.db)
+			.collection(dbinfo.usersCollection)
+			.findOne({"_id": oid}, function (err,data) {
+				if (err) {
+					userClient.close();
+					console.log(err);
+					res.sendStatus = 400;
+					console.log('1');
+					res.send({status: "error"});
+				} else {
+					const newMoney = data.money + count;
+					const newvalues = { $set: {money: newMoney}};
+					userClient
+						.db(dbinfo.db)
+						.collection(dbinfo.usersCollection)
+						.updateOne({"_id": oid}, newvalues, function (err, res) {
+							if (err) {
+								userClient.close();
+								console.log(err);
+								res.sendStatus = 400;
+								console.log('2');
+								res.send({status: "error"});
+							} else {
+								userClient.close();
+								res.sendStatus = 200;
+								sendOk();
+							}
+						})
+				}
+			});
+	});
 });
 
 module.exports = router;
