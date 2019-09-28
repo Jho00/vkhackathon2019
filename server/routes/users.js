c38d2b8cbb150791bd37fb7ded71b89b9867b203
Jambo <jambotoyou@gmail.com>
2019-09-28 16:45:18 +0400

    fetched user data from vkapi

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,12 +2,8 @@ var axios = require('axios');
 var express = require('express');
 var router = express.Router();
 var dbinfo = require('./dbinfo');
+var getUserData = require('./vkapi/user').getUserData;
 var DBClinet = require('mongodb').MongoClient;
-
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
-});
 
 router.get('/auth', function (req, res, next) {
 	let code = req.query.code;
@@ -21,39 +17,54 @@ router.get('/auth', function (req, res, next) {
 		}
 	}).then(json => {
 
+		console.log("----------");
+		console.log(json.data);
+		console.log("----------");
+
 		let token = json.data.access_token;
+		let user_id = json.data.user_id.toString();
 		let done = false;
 
-		DBClinet.connect(dbinfo.connStr, function(err, userClient) {
+		var db = new DBClinet(dbinfo.connStr, { useNewUrlParser: true, useUnifiedTopology: true });
+
+		db.connect(function(err, userClient) {
+			if (err) {
+				userClient.close();
+				res.send({ status: "error" });
+			}
+
 			userClient
 				.db(dbinfo.db)
 				.collection(dbinfo.usersCollection)
-				.find({ "site_token": token })
-				.toArray(function (err, data) {
-					if (err) {
-						console.log(err);
-						res.sendStatus = 400;
+				.findOne({ "_id": user_id }, function (error, data) {
+					
+					if (error) {
+						userClient.close();
 						res.send({ status: "error" });
-					} else if (data.length > 0) {
-						res.send({ status: "success", data: data[0]._id })
-						done = true;
+					} else if (data) {
+						userClient.close();
+						res.send({ status: "success", data: data._id })
+					} else {
+						userClient
+							.db(dbinfo.db)
+							.collection(dbinfo.usersCollection)
+							.insertOne({
+									"_id": json.data.user_id.toString(),
+									"site_token": token,
+									"money": 1000
+								}, function(err, data) {
+								if (err) {
+									userClient.close();
+									res.send({ status: "error" });
+								}
+
+								userClient.close();
+								res.send({ status: "success", data: data.insertedId });
+							})
 					}
 				});
 			
 			if (!done) {
-				userClient
-					.db(dbinfo.db)
-					.collection(dbinfo.usersCollection)
-					.insertOne({ "site_token": token, "money": 0 }, function(err, data) {
-						if (err) {
-							console.log(err);
-							res.sendStatus = 400;
-							res.send({ status: "error" });
-						}
-
-						console.log(data.insertedId);
-						res.send({ status: "success", data: data.insertedId });
-					})
 				
 			}
 
@@ -61,11 +72,54 @@ router.get('/auth', function (req, res, next) {
 		});
 	})
 		.catch(err => {
-			console.log(err);
 			res.send({
 				status: "error"
 			});
 		});
+});
+
+/* GET users listing. */
+router.get('/:id', function (req, res, next) {
+	let user_id = req.params.id;
+
+	if (user_id == "auth") {
+		next();
+	}
+
+	DBClinet.connect(
+			dbinfo.connStr, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true
+			}, function (err, client) {
+
+			if (err) {
+				res.send({status: "error"});
+			}
+
+			client
+				.db(dbinfo.db)
+				.collection(dbinfo.usersCollection)
+				.findOne({"_id": user_id}, function (error, { site_token, money }) {
+
+					if (error) {
+						res.send({status: "error"});
+						client.close();
+					}
+
+					getUserData(site_token, user_id).then(usrdb => {
+
+						res.send({
+							status: "success",
+							data: {
+								...usrdb.data.response[0],
+								money
+							}});
+						client.close();
+					})
+				})
+	})
+
+	getUserData()
 });
 
 module.exports = router;
