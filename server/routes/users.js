3368e0494685c32922a9ec4390a373e5e7f7fc3f
Akutin Artem <akutin_artem@mail.ru>
2019-09-28 12:59:14 +0400

    User routes (#5)
    * add user money
    
    * get info

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 var dbinfo = require('./dbinfo');
 var DBClinet = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -44,7 +45,7 @@ router.get('/auth', function (req, res, next) {
 				userClient
 					.db(dbinfo.db)
 					.collection(dbinfo.usersCollection)
-					.insertOne({ "site_token": token, "money": 0 }, function(err, data) {
+					.insertOne({ "site_token": token, "money": 1000 }, function(err, data) {
 						if (err) {
 							console.log(err);
 							res.sendStatus = 400;
@@ -66,6 +67,77 @@ router.get('/auth', function (req, res, next) {
 				status: "error"
 			});
 		});
+});
+
+router.post('/add-money', function (req, res, next) {
+	const count = Number(req.query.count);
+	const id = req.query.user_id;
+	const sendOk = function() {
+		res.send({status: "ok"});
+	};
+	const oid = new ObjectId(id);
+	DBClinet.connect(dbinfo.connStr, function(err, userClient) {
+		userClient
+			.db(dbinfo.db)
+			.collection(dbinfo.usersCollection)
+			.findOne({"_id": oid}, function (err,data) {
+				if (err) {
+					userClient.close();
+					res.sendStatus = 400;
+					res.send({status: "error"});
+				} else {
+					const newMoney = data.money + count;
+					const newvalues = { $set: {money: newMoney}};
+					userClient
+						.db(dbinfo.db)
+						.collection(dbinfo.usersCollection)
+						.updateOne({"_id": oid}, newvalues, function (err, res) {
+							if (err) {
+								userClient.close();
+								res.sendStatus = 400;
+								res.send({status: "error"});
+							} else {
+								userClient.close();
+								res.sendStatus = 200;
+								sendOk();
+							}
+						})
+				}
+			});
+	});
+});
+
+router.get('/info', function (req, res, next) {
+	const id = req.query.user_id;
+	const oid = new ObjectId(id);
+	const sendOk = function() {
+		res.send({status: "ok"});
+	};
+	const user = {
+		name: '',
+		avatar_url: '',
+		money: '',
+		challenge_finished: '',
+		challenge_created: '',
+		challenge_now: ''
+	};
+	DBClinet.connect(dbinfo.connStr, function(err, userClient) {
+		userClient
+			.db(dbinfo.db)
+			.collection(dbinfo.usersCollection)
+			.findOne({"_id": oid}, function (err,data) {
+				if (err) {
+					userClient.close();
+					res.sendStatus = 400;
+					res.send({status: "error"});
+				} else {
+					user.name = data.name || 'not present';
+					user.avatar_url = data.avatar_url || 'not present';
+					user.money = data.money || 'not present';
+					res.send({user: user});
+				}
+			})
+	})
 });
 
 module.exports = router;
